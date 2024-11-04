cfc9cdf4b9fbf2665705167b3fbbb562381b8a8a
William Bennett <wbennett@lanl.gov>
2024-11-04 15:27:10 -0700

    small potatoes

--- cc_vs_gauss.py
+++ cc_vs_gauss.py
@@ -9,16 +9,16 @@ def RMSE(l1,l2):
     return np.sqrt(np.mean((l1-l2)**2))
 
 def perform_convergence():
-    N_ang_list = np.array([2,6,16,46,136])
+    N_ang_list = np.array([2,6,16,46])
     cc_err = np.zeros((3, N_ang_list.size))
     gauss_err = np.zeros((3, N_ang_list.size))
     N_cells = 100
-    psib, phib, cell_centersb, musb, tableaub, Jb = solve(N_cells = N_cells, N_ang = 512, left_edge = 'source1', right_edge = 'source1', IC = 'cold', source = 'off',
+    psib, phib, cell_centersb, musb, tableaub, Jb = solve(N_cells = N_cells, N_ang = 256, left_edge = 'source1', right_edge = 'source1', IC = 'cold', source = 'off',
             opacity_function = 'constant', wynn_epsilon = False, laststep = False,  L = 5.0, tol = 1e-13, source_strength = 1.0, sigma_a = 0.0, sigma_s = 1.0, sigma_t = 1.0,  strength = [1.0,0.0], maxits = 1e10, input_source = np.array([0.0]), quad_type='gauss')
 
     for iang, ang in tqdm.tqdm(enumerate(N_ang_list)):
         psicc, phicc, cell_centerscc, muscc, tableaucc, Jcc = solve(N_cells = N_cells, N_ang = ang, left_edge = 'source1', right_edge = 'source1', IC = 'cold', source = 'off',
-            opacity_function = 'constant', wynn_epsilon = True, laststep = False,  L = 5.0, tol = 1e-13, source_strength = 1.0, sigma_a = 0.0, sigma_s = 1.0, sigma_t = 1.0,  strength = [1.0,0.0], maxits = 1e10, input_source = np.array([0.0]))
+            opacity_function = 'constant', wynn_epsilon = True, laststep = True,  L = 5.0, tol = 1e-13, source_strength = 1.0, sigma_a = 0.0, sigma_s = 1.0, sigma_t = 1.0,  strength = [1.0,0.0], maxits = 1e10, input_source = np.array([0.0]))
         
         psig, phig, cell_centersg, musg, tableaug, Jg = solve(N_cells = N_cells, N_ang = ang, left_edge = 'source1', right_edge = 'source1', IC = 'cold', source = 'off',
             opacity_function = 'constant', wynn_epsilon = False, laststep = False,  L = 5.0, tol = 1e-13, source_strength = 1.0, sigma_a = 0.0, sigma_s = 1.0, sigma_t = 1.0,  strength = [1.0,0.0], maxits = 1e10, input_source = np.array([0.0]), quad_type = 'gauss')
@@ -35,22 +35,23 @@ def perform_convergence():
     err_estimate = np.zeros(N_ang_list.size)
     for ix in range(N_cells):
         phi_err_estimate[:, ix] = estimate_error(N_ang_list, tableaucc)
-    for ang in range(N_ang_list):
-        err_estimate = RMSE(phi_err_estimate[ang,:], phi_err_estimate[ang,:]*0)
+    for ang in range(N_ang_list.size):
+        err_estimate[ang] = RMSE(phi_err_estimate[ang,:], phi_err_estimate[ang,:]*0)
 
     plt.figure('Scalar flux')
     plt.loglog(N_ang_list, cc_err[0], '-^', mfc = 'none')
     plt.loglog(N_ang_list, gauss_err[0], '-o', mfc = 'none')
     plt.loglog(N_ang_list, err_estimate, '-s', mfc = 'none')
+    print(err_estimate)
     plt.savefig('flux_converge.pdf')
     plt.show()
 
     plt.figure('J')
-    plt.loglog(N_ang_list, cc_err[1], 'r-^', mfc = 'none',  label = 'left')
-    plt.loglog(N_ang_list, gauss_err[1], 'b-o', mfc = 'none',  label = 'left')
+    plt.loglog(N_ang_list, cc_err[1], 'r--^', mfc = 'none',  label = 'left')
+    plt.loglog(N_ang_list, gauss_err[1], 'b--o', mfc = 'none',  label = 'left')
     plt.loglog(N_ang_list, cc_err[2], 'g-^', mfc = 'none',  label = 'right')
     plt.loglog(N_ang_list, gauss_err[2], 'y-o', mfc = 'none', label = 'right')
-    plt.legend()
+    # plt.legend()
     plt.savefig('J_converge.pdf')
     plt.show()
 
@@ -58,5 +59,6 @@ def perform_convergence():
 def estimate_error(ang_list, tableau):
     err_estimate = np.zeros(ang_list.size)
     for ia in range(2, ang_list.size):
+        print(tableau[1:, 1][0:ia])
         err_estimate[ia] = convergence_estimator(ang_list[0:ia], tableau[1:, 1][0:ia])
     return err_estimate
